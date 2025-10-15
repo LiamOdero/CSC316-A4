d332741e7090a6b429461d1c7f7f9707a43c5898
Liam Odero <liam.odero1@gmail.com>
2025-10-15 16:45:00 -0400

    data cleansing completed

--- js/main.js
+++ js/main.js
@@ -3,35 +3,43 @@
 let areachart, timeline;
 let data;
 
+let RADIUS_EARTH = 6378;
+let SUN_LUMINOSITY = 3.83e26
+
 // Start application by loading the data
 loadData();
 
 function loadData() {
-    d3.json("data/star_dataset.csv"). then(data=>{
+    d3.csv("data/star_dataset.csv"). then(data=>{
             
         data = prepareData(data)
         
         console.log('data loaded ')
 
-        // TO-DO (Activity I): instantiate visualization objects
-		areachart = new StarDisplayChart("stacked-area-chart", data.layers);
+		areachart = new StarDisplayChart("stacked-area-chart", data);
 
-        // TO-DO (Activity I):  init visualizations
 		areachart.initVis();
 
 		minimap = new Minimap("timeline", data.years)
 		minimap.initVis();
-
     });
 
 }
 
-
-// helper function - PROVIDE WITH TEMPLATE
 function prepareData(data){
-
+	data_cleansed = [];
+	data.forEach(e => {
+		data_cleansed.push({name: e.Name, 
+							dist: +e["Distance (ly)"], 
+							lum: +e["Luminosity (L/Lo)"] * SUN_LUMINOSITY, 
+							rad: +e["Radius (R/Ro)"] * RADIUS_EARTH,
+							temp: +e["Temperature (K)"],
+							specClass: e["Spectral Class"]})
+	});
+	console.log(data_cleansed)
 	return data
 }
 
 function brushed() {
+
 }
